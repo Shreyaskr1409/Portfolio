7b4aec86ff94fca1a0c05e05da8ccb094fa6c800
Shreyaskr1409 <shreyaskr.0914@gmail.com>
2024-05-23 21:55:35 +0530

    Few Changes

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const spacing1 = document.getElementById("spacing1")
-// const left_box = document.getElementById("left_box")
 
 
 navbar.setAttribute("style", "height: "+(document.getElementById("navbar_inner").clientHeight+2)+"px;")
-spacing1.setAttribute("style", "height: "+(document.getElementById("navbar").clientHeight + 10)+"px;")
+spacing1.setAttribute("style", "height: "+(document.getElementById("navbar").clientHeight)+"px;")
 
 addEventListener("resize",() => {
     navbar.setAttribute("style", "height: "+(document.getElementById("navbar_inner").clientHeight+2)+"px;")
-    spacing1.setAttribute("style", "height: "+(navbar.clientHeight + 10)+"px;")
+    spacing1.setAttribute("style", "height: "+(navbar.clientHeight)+"px;")
 })
