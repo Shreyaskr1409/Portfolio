c23907dc4c561e57df7c82c8f3bdfa4b789dbed1
Shreyaskr1409 <shreyaskr.0914@gmail.com>
2024-05-25 18:47:29 +0530

    Loading animation added

--- index.js
+++ index.js
@@ -8,3 +8,43 @@ addEventListener("resize",() => {
     navbar.setAttribute("style", "height: "+(document.getElementById("navbar_inner").clientHeight+2)+"px;")
     spacing1.setAttribute("style", "height: "+(navbar.clientHeight)+"px;")
 })
+
+
+
+
+function startLoader() {
+    let counterElement = document.querySelector("#count2");
+    let currentValue = 0;
+
+    function updateCounter() {
+        if(currentValue === 100) {
+            gsap.to(".counter", 0.25, {
+                opacity: 0
+            });
+            gsap.to(".bar", 1.5, {
+                height: 0,
+                stagger: {
+                    amount: 0.5
+                },
+                ease: "power4.inOut"
+            });
+            gsap.to(".counter", 0.25, {
+                delay: 1,
+                height: 0
+            });
+            return;
+        }
+        currentValue += Math.floor(Math.random() * 10) + 1;
+
+        if(currentValue > 100) {
+            currentValue = 100;
+        }
+
+        counterElement.textContent = currentValue;
+        let delay = Math.floor(Math.random()*200) + 120;
+        setTimeout(updateCounter, delay);
+    }
+    updateCounter();
+}
+
+startLoader();
